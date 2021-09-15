ffe07cea2e4c599e195d45f9f681f42f04588b22
Rauqeraa <rauqeraa@yahoo.com>
2021-09-15 16:32:19 +0530

    commmit customer

---
+++ ProjectWork/src/CustomerDetails.java
@@ -0,0 +1,17 @@
+
+public class CustomerDetails{
+	String DateReceived;
+	String Product;
+	String SubProduct;	
+	String Issue;
+	String SubIssue;	
+	String Company;
+	String State;
+	String ZIPcode;
+	String Submittedvia;	
+	String DateSentToCompany;	
+	String CompanyResponseToConsumer;	
+	String TimelyResponse;
+	String ConsumerDisputed;
+	String ComplaintID;
+}
